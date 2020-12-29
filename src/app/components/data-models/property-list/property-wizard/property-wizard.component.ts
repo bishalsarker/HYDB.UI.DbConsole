36151be0f81731bddd81f77ebd93ca97a8e273e6
Bishal Sarker <mr.bishalsarker@gmail.com>
2020-12-30 00:11:28 +0600

    restricted keywords added

--- src/app/components/data-models/property-list/property-wizard/property-wizard.component.ts
+++ src/app/components/data-models/property-list/property-wizard/property-wizard.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { IDataModelProperty } from 'src/app/interfaces/IDataModel';
 import { PropertyListComponent } from '../property-list.component';
+import { includes } from 'lodash';
 
 @Component({
   selector: 'hydb-property-wizard',
@@ -17,6 +17,8 @@ export class PropertyWizardComponent implements OnInit {
     Validators.required,
     Validators.pattern('^[a-zA-Z_]*$')
   ]);
+  public blockedKeywords: string[] = ['id'];
+  public hasBlockedKeywordError: boolean = false;
 
   public propertyType: FormControl = new FormControl('', Validators.required);
 
@@ -31,12 +33,27 @@ export class PropertyWizardComponent implements OnInit {
       this.propertyName.setValue(this.data.propertyModel.name);
       this.propertyType.setValue(this.data.propertyModel.type);
     }
+
+    this.propertyName.valueChanges.subscribe(val => {
+      this.hasBlockedKeywordError = false;
+    });
   }
 
   public onCreateClick(): void {
     this.data.propertyModel.name = this.propertyName.value;
     this.data.propertyModel.type = this.propertyType.value;
-    this.dialogRef.close(this.data);
+
+    if(this.hasBlockedKeyword(this.data.propertyModel.name)) {
+      this.hasBlockedKeywordError = true;
+    }
+
+    if(!this.hasBlockedKeywordError) {
+      this.dialogRef.close(this.data);
+    }
+  }
+
+  private hasBlockedKeyword(keyword: string): boolean {
+    return includes(this.blockedKeywords, keyword);
   }
 
 }
